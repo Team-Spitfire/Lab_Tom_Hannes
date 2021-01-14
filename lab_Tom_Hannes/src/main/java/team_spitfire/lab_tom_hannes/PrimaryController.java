50e3274461e6dbe276560e508f406e875f5f8e9b
XSilentstorm <47358890+XSilentstorm@users.noreply.github.com>
2021-01-14 14:23:54 +0100

    SET-UP
    project setup

---
+++ lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/PrimaryController.java
@@ -0,0 +1,12 @@
+package team_spitfire.lab_tom_hannes;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/SecondaryController.java
@@ -0,0 +1,12 @@
+package team_spitfire.lab_tom_hannes;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
