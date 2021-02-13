1af4b933c955696892fe1f859cb527df7ba2df56
XSilentstorm <47358890+XSilentstorm@users.noreply.github.com>
2021-02-13 17:24:41 +0100

    delete
    delete priamry secondary

--- lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/GameViewController.java
+++ lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/GameViewController.java
@@ -7,7 +7,9 @@ package team_spitfire.lab_tom_hannes;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -15,6 +17,75 @@ import javafx.fxml.Initializable;
  * @author hanne
  */
 public class GameViewController implements Initializable {
+
+    @FXML
+    private ImageView iv1_2;
+    @FXML
+    private ImageView iv1_4;
+    @FXML
+    private ImageView iv1_5;
+    @FXML
+    private ImageView iv2_1;
+    @FXML
+    private ImageView iv2_2;
+    @FXML
+    private ImageView iv2_3;
+    @FXML
+    private ImageView iv2_4;
+    @FXML
+    private ImageView iv2_5;
+    @FXML
+    private ImageView iv2_6;
+    @FXML
+    private ImageView iv2_7;
+    @FXML
+    private ImageView iv3_2;
+    @FXML
+    private ImageView iv3_4;
+    @FXML
+    private ImageView iv3_6;
+    @FXML
+    private ImageView iv4_1;
+    @FXML
+    private ImageView iv4_2;
+    @FXML
+    private ImageView iv4_3;
+    @FXML
+    private ImageView iv4_4;
+    @FXML
+    private ImageView iv4_5;
+    @FXML
+    private ImageView iv4_6;
+    @FXML
+    private ImageView iv4_7;
+    @FXML
+    private ImageView iv5_2;
+    @FXML
+    private ImageView iv5_4;
+    @FXML
+    private ImageView iv5_6;
+    @FXML
+    private ImageView iv6_1;
+    @FXML
+    private ImageView iv6_2;
+    @FXML
+    private ImageView iv6_3;
+    @FXML
+    private ImageView iv6_4;
+    @FXML
+    private ImageView iv6_5;
+    @FXML
+    private ImageView iv6_6;
+    @FXML
+    private ImageView iv6_7;
+    @FXML
+    private ImageView iv7_2;
+    @FXML
+    private ImageView iv7_4;
+    @FXML
+    private ImageView iv7_6;
+    @FXML
+    private ImageView ivNewCard;
 
     /**
      * Initializes the controller class.
