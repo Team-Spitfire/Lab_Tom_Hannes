b0664e11554483a2bd0cdd9f8d3a3569f29831fa
XSilentstorm <47358890+XSilentstorm@users.noreply.github.com>
2021-02-13 17:05:53 +0100

    base

--- lab_Tom_Hannes/src/main/java/module-info.java
+++ lab_Tom_Hannes/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module team_spitfire.lab_tom_hannes {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens team_spitfire.lab_tom_hannes to javafx.fxml;
     exports team_spitfire.lab_tom_hannes;
