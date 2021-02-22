c026253c83465b115215903a684d281f30a194fa
XSilentstorm <47358890+XSilentstorm@users.noreply.github.com>
2021-02-22 15:53:31 +0100

    Update GameViewController.java

--- lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/GameViewController.java
+++ lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/GameViewController.java
@@ -100,58 +100,72 @@ public class GameViewController implements Initializable {
 
     @FXML
     private void btnTurnRight(ActionEvent event) {
+        App.getNewField().turnRight();
     }
 
     @FXML
     private void btnTurnLeft(ActionEvent event) {
+        App.getNewField().turnRight();
     }
 
     @FXML
     private void btn10insU(ActionEvent event) {
+        App.PfeilY1Oben(F);
     }
 
     @FXML
     private void btn30insU(ActionEvent event) {
+        App.PfeilY3Oben(F);
     }
 
     @FXML
     private void btn50insU(ActionEvent event) {
+        App.PfeilY5Oben(F);
     }
 
     @FXML
     private void btn61insR(ActionEvent event) {
+        App.PfeilX1Rechts(F);
     }
 
     @FXML
     private void btn63insR(ActionEvent event) {
+     App.PfeilX3Rechts(F);
     }
 
     @FXML
     private void btn65insR(ActionEvent event) {
+        App.PfeilX5Rechts(F);
     }
 
     @FXML
     private void btn56insD(ActionEvent event) {
+        App.PfeilY5Unten(F);
     }
 
     @FXML
     private void btn36insD(ActionEvent event) {
+        App.PfeilY3Unten(F);
     }
 
     @FXML
     private void btn16insD(ActionEvent event) {
+        App.PfeilY1Unten(F);
     }
 
     @FXML
     private void btn01insL(ActionEvent event) {
+        App.PfeilX1Links(F);
     }
 
     @FXML
     private void btn03insL(ActionEvent event) {
+        App.PfeilX3Links(F);
     }
 
     @FXML
     private void btn05insL(ActionEvent event) {
+        App.PfeilX5Links(F);
     }
     
     /**
