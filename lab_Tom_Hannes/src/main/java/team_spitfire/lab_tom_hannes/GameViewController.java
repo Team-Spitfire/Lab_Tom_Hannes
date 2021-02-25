3e8f09286405038971d791abd3a0b6c4abdfddb5
XSilentstorm <47358890+XSilentstorm@users.noreply.github.com>
2021-02-25 14:43:55 +0100

    Update GameViewController.java

--- lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/GameViewController.java
+++ lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/GameViewController.java
@@ -250,9 +250,7 @@ public class GameViewController implements Initializable {
         int X = 0;
         int Y = 0;
         boolean exit = false;
-        
-        System.out.println(App.field[0][0]);
-        
+
         while(exit == false){
             FieldModel F = App.field[X][Y];
             Image i = F.getImg();
