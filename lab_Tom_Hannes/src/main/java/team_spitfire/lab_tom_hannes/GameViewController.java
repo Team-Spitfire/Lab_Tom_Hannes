24150f6b9847ec2299efb707e748c191408d74ad
XSilentstorm <47358890+XSilentstorm@users.noreply.github.com>
2021-02-26 14:16:44 +0100

    Update GameViewController.java

--- lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/GameViewController.java
+++ lab_Tom_Hannes/src/main/java/team_spitfire/lab_tom_hannes/GameViewController.java
@@ -302,25 +302,19 @@ public class GameViewController implements Initializable {
 
         int x = cPlayer.getPosX();
         int y = cPlayer.getPosY();
-        FieldModel d = null;
-        FieldModel u = null;
-        FieldModel l = null;
-        FieldModel r = null;
-
+        
+        
+        FieldModel d = App.field[x][y + 1];
+        FieldModel u = App.field[x][y - 1];
+        FieldModel l = App.field[x - 1][y];
+        FieldModel r = App.field[x + 1][y];
+        
+        
+        
         //Alle Fieldmodels die den Spieler Umgeben
         FieldModel c = App.field[x][y];
-        if(x != 0 ){
-            d = App.field[x - 1][y];
-        }
-        else if(x != 6){
-            u = App.field[x + 1][y];
-        }
-        else if(y != 0){
-            l = App.field[x][y - 1];
-        }
-        else if(y != 6){
-            r = App.field[x][y + 1];
-        }
+        
+       
 
         //Alle Booleans des Feldes wo der Spieler sich drauf befindet
         boolean cu = c.isUp();
